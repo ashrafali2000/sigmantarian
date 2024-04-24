ff611a84d7ae02ce907cf152efebe41f06dac20f
AREEBA-MUHAMMAD-AIJAZ <96327834+AREEBA-MUHAMMAD-AIJAZ@users.noreply.github.com>
2024-04-24 22:15:59 +0500

    set

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -76,10 +76,24 @@ export default function Example() {
                   src="./search.png"
                   className="w-8 rounded-full z-40  cursor-pointer hidden md:block"
                 />
-                <img
+
+                <a
+                  href="#_"
+                  class="relative px-5 py-3 overflow-hidden font-bold text-[#541d69] bg-gray-100 border border-gray-100 rounded-lg shadow-inner group"
+                >
+                  <span CLASS="absolute top-0 left-0 w-0 h-0 transition-all duration-200 border-t-2 border-gray-600 group-hover:w-full ease"></span>
+                  <span CLASS="absolute bottom-0 right-0 w-0 h-0 transition-all duration-200 border-b-2 border-gray-600 group-hover:w-full ease"></span>
+                  <span CLASS="absolute top-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
+                  <span CLASS="absolute bottom-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
+                  <span CLASS="absolute inset-0 w-full h-full duration-300 delay-300 bg-[#752a92] opacity-0 group-hover:opacity-100"></span>
+                  <span CLASS="relative transition-colors duration-300 delay-200 group-hover:text-white ease">
+                    MENU
+                  </span>
+                </a>
+                {/* <img
                   src="./metamask2.png"
                   className="w-14 rounded-full z-40 hidden md:block"
-                />
+                /> */}
                 {/* Profile dropdown */}
                 {/* <Menu as="div" className="relative ml-3">
                   <div>
